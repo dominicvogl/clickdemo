72d24afaece66f5f4ff2cd50453cc0ee56ee9275
dvcccc <dv@cccc.de>
2012-09-06 15:35:57 +0200

    [START]

---
+++ header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+	<head>
+	
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	
+	    <!--
+	    # 	Klickdemo Framework
+	    #	(c) 2012 Catalyst Interactive | Dominic Vogl
+	    #	http://www.cccc.de
+	    #
+	    #	Version 1.0.2 
+	    -->
+	    
+		<?php #EDIT HERE TITLE ?>
+		<title>PREVIEW - Your Title</title>
+		
+	    <?php #LOAD EXTERN CSS FILES ?>
+		<link href="css/reset.css" rel="stylesheet" type="text/css" media="screen"  />
+	    <link href="css/master.css" rel="stylesheet" type="text/css" media="screen"  />
+	
+	</head>
+	
+	<body>
+	
+	<div class="nav">
+	
+		<?php #BUILD YOUR NAVIGATION ?>
+	    <ul>
+	    	<li><a href="index.php">Your Title</a></li>
+	    	<li><a href="your_file_name.php">Your Title</a></li>   	
+	    	<li><a href="your_file_name.php">Your Title</a></li>
+	    </ul>
+	
+	</div><?php #NAV END ?>
---
+++ index.php
@@ -0,0 +1,10 @@
+
+<?php include('header.php');?>
+
+	<div class="image">
+    
+    	<? INSERT HERE YOUR SCREEN ?>
+        <img src="elemente/your_file_name.jpg" />
+
+</body>
+</html>
